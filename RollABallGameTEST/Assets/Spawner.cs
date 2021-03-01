f7fe448166b773568b2d0cd08df7647447db3dbc
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2021-03-01 17:32:23 +0100

    rings now spawn at start and move in camera direction

--- RollABallGameTEST/Assets/Spawner.cs
+++ RollABallGameTEST/Assets/Spawner.cs
@@ -1,18 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using System;
 
 public class Spawner : MonoBehaviour
 {
+    public float velocity;
+    public float acceleration;
+    public float destroyPoint;
+    public float spawnPoint;
+    public GameObject ringType1;
+    public List<GameObject> ringsInLevel = new List<GameObject>();
+    public List<GameObject> ringsLoaded = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        float ringWidth = 2*(ringType1.GetComponent<MeshRenderer>().bounds.extents.z);
+        for (int i = 0; i<10 ; i++)
+        {
+            ringsInLevel.Add(ringType1);
+        }
+
+        int ringsAtStart = (int)(System.Math.Abs(spawnPoint - destroyPoint) / ringWidth);
+        for (int i = 0; i<ringsAtStart; i++)
+        {
+            ringsLoaded.Add(Instantiate(ringsInLevel[i], new UnityEngine.Vector3(0, 0, destroyPoint+i*ringWidth), UnityEngine.Quaternion.identity)) ;
+
+        }
+        Debug.Log(ringsLoaded.Count);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for(int i = 0; i<=ringsLoaded.Count; i++)
+        {
+            ringsLoaded[i].transform.position = new UnityEngine.Vector3(0,0, ringsLoaded[i].transform.position.z - velocity * Time.deltaTime);
+        }
+
+        float newSpeed = velocity += acceleration * Time.deltaTime;
+        velocity = newSpeed;
+
     }
 }
